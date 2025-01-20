19769fc0a05d4bd3b4eeaff49a6c4f12a481d51b
Nicolas Accetta <nicolas.accetta@gmail.com>
2025-01-19 22:10:27 -0300

    chore: ignore lsp cache files

--- src/functions/core.clj
+++ src/functions/core.clj
@@ -1,14 +1,10 @@
 (ns functions.core)
-(defn greet [] (println "Hello"))
-(defn greeting 
-  ([] (greeting "World"))
-  ([greeter] (
-              let [result (str "Hello, " greeter "!")]
-              (println result)
-              result))
-  ([greet greeter] (
-              let [result (str greet ", " greeter "!")]
-              (println result)
-              result))
-)
 
+(defn greeting
+  ([] (greeting "World"))
+  ([greeter] (let [result (str "Hello, " greeter "!")]
+               (println result)
+               result))
+  ([greet greeter] (let [result (str greet ", " greeter "!")]
+                     (println result)
+                     result)))
