48636f3493a5a41206b08b5db93caf9123f3e2ae
Nicolas Accetta <nicolas.accetta@gmail.com>
2025-01-23 17:47:08 -0300

    test: remove deprecated case

--- src/transform/test.clj
+++ src/transform/test.clj
@@ -1,6 +1,6 @@
 
 (ns transform.test
-  (:require [transform.core :refer [parse-json, get-definition, get-array-types, make-type]]
+  (:require [transform.core :refer [parse-json, get-definition, make-type]]
             [clojure.pprint :as pp]))
 
 (assert (some? (parse-json)))
@@ -8,9 +8,6 @@
 
 (assert (some? (get-definition "StringSet" (parse-json))))
 (pp/pprint (get-definition "StringSet" (parse-json)))
-
-(assert (= '("string") (get-array-types (get-definition "StringSet" (parse-json)))))
-(pp/pprint (get-array-types (get-definition "StringSet" (parse-json))))
 
 (pp/pprint (make-type (get-definition "StringSet" (parse-json))))
 (assert (= "string[]" (make-type (get-definition "StringSet" (parse-json)))))
