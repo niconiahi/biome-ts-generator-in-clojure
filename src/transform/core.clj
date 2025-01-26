f01b5427dc2a0172fa8383474cb57d02c0356750
Nicolas Accetta <nicolas.accetta@gmail.com>
2025-01-26 14:59:30 -0300

    feat: add parse object function + handle type key

--- src/transform/core.clj
+++ src/transform/core.clj
@@ -11,7 +11,32 @@
 (defn get-definition [key schema]
   (get-in schema [:definitions (keyword key)]))
 
-(defn make-type [definition] (let [type (get definition :type)]
-                               (cond
-                                 (= type "array") (str (join "" (vals (get definition :items))) "[]")
-                                 (= type "number") (str "(" ")[]"))))
+(defn parse-array [definition]
+  (str (join "" (vals (get definition :items))) "[]"))
+
+(defn parse-object [definition]
+  (let
+   [properties (get definition :properties)]
+    (reduce
+     (fn [accumulator [n, property]]
+       (let [type (get property :type)
+             any-of (get property :anyOf)]
+         (prn "name is" n)
+         (prn "current accumulator is" accumulator)
+         (prn "property type is" type)
+         (prn "property anyof is" any-of)
+         (prn "is string" (string? type))
+         (prn "is vector" (vector? type))
+         (prn "is anyof" (vector? any-of))
+         (cond
+           (string? type) (conj accumulator (str (name n) ": " type "\n"))
+           (vector? type) (conj accumulator (str (name n) ": " (join " | " type) "\n"))
+           (vector? any-of) (println "i'm a any-of structure that needs to be parsed"))))
+     []
+     properties)))
+
+(defn make-type [definition]
+  (let [type (get definition :type)]
+    (cond
+      (= type "array") (parse-array definition)
+      (= type "object") (parse-object definition))))
--- src/transform/test.clj
+++ src/transform/test.clj
@@ -1,13 +1,62 @@
 
 (ns transform.test
-  (:require [transform.core :refer [parse-json, get-definition, make-type]]
+  (:require [transform.core :refer [parse-json, get-definition, make-type, parse-object]]
             [clojure.pprint :as pp]))
 
 (assert (some? (parse-json)))
-(pp/pprint (get-definition "StringSet" (parse-json)))
+; (pp/pprint (parse-json))
 
 (assert (some? (get-definition "StringSet" (parse-json))))
-(pp/pprint (get-definition "StringSet" (parse-json)))
+; (pp/pprint (get-definition "StringSet" (parse-json)))
 
-(pp/pprint (make-type (get-definition "StringSet" (parse-json))))
 (assert (= "string[]" (make-type (get-definition "StringSet" (parse-json)))))
+; (pp/pprint (make-type (get-definition "StringSet" (parse-json))))
+
+; (pp/pprint (make-type (get-definition "AssistsConfiguration" (parse-json))))
+
+; (pp/pprint (parse-object {:properties {:type "string"}}))
+(pp/pprint (parse-object {:AssistConfiguration
+                          {:type "object",
+                           :properties
+                           {:enabled
+                            {:description "Whether Biome should enable assists via LSP.",
+                             :type "boolean"}},
+                           :additionalProperties false}}))
+(assert (= "type AssistConfiration = {
+  enabled: boolean
+}" (parse-object {:AssistConfiguration
+                  {:type "object",
+                   :properties
+                   {:enabled
+                    {:description "Whether Biome should enable assists via LSP.",
+                     :type "boolean"}},
+                   :additionalProperties false}})))
+
+; (pp/pprint (parse-object {:type "object",
+;                           :properties
+;                           {:enabled
+;                            {:description "Whether Biome should enable assists via LSP.",
+;                             :type "boolean"}},
+;                           :additionalProperties false}))
+;
+; (pp/pprint (parse-object {:type "object",
+;                           :properties
+;                           {:include
+;                            {:description
+;                             "A list of Unix shell style patterns. The formatter will include files/folders that will match these patterns.",
+;                             :anyOf [{:$ref "#/definitions/StringSet"} {:type "null"}]}},
+;                           :additionalProperties false}))
+;
+; (pp/pprint (parse-object {:type "object",
+;                           :properties
+;                           {:enabled
+;                            {:description "Whether Biome should enable assists via LSP.",
+;                             :type "boolean"},
+;                            :ignore
+;                            {:description "Whether Biome should enable assists via LSP.",
+;                             :type ["boolean", "string"]},
+;                            :include
+;                            {:description
+;                             "A list of Unix shell style patterns. The formatter will include files/folders that will match these patterns.",
+;                             :anyOf [{:$ref "#/definitions/StringSet"} {:type "null"}]}},
+;                           :additionalProperties false}))
