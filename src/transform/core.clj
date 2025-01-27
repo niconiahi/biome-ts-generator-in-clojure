659408cb9835412568b3ce3ae7eda3f392286947
Nicolas Accetta <nicolas.accetta@gmail.com>
2025-01-27 18:22:11 -0300

    feat: parses any of type found in object

--- src/transform/core.clj
+++ src/transform/core.clj
@@ -1,6 +1,8 @@
 (ns transform.core
   (:require [clojure.data.json :refer [read-str]]
-            [clojure.string :refer [join]]))
+            [clojure.string :refer [join, split]]))
+
+(declare make-type)
 
 (defn parse-json []
   (-> "src/transform/schema.json"
@@ -14,28 +16,50 @@
 (defn parse-array [definition]
   (str (join "" (vals (get definition :items))) "[]"))
 
+(defn get-definition-name [ref-string]
+  (last (split ref-string #"/")))
+
 (defn parse-object [definition n]
   (let
    [properties (get definition :properties)]
     (join "\n"
-          (concat [(join " " ["type" n "=" "{"])]
-                  (reduce
-                   (fn [accumulator [n, property]]
-                     (let [type (get property :type)
-                           any-of (get property :anyOf)]
-                       (prn "name is" n)
-                       (prn "current accumulator is" accumulator)
-                       (prn "property type is" type)
-                       (prn "property anyof is" any-of)
-                       (prn "is string" (string? type))
-                       (prn "is vector" (vector? type))
-                       (prn "is anyof" (vector? any-of))
-                       (cond
-                         (string? type) (conj accumulator (str "  " (name n) ": " type))
-                         (vector? type) (conj accumulator (str "  " (name n) ": " (join " | " type)))
-                         (vector? any-of) accumulator)))
-                   []
-                   properties) ["}"]))))
+          (concat
+           [(join " " ["type" n "=" "{"])]
+           (reduce
+            (fn [accumulator [n, property]]
+              (let [type (get property :type)
+                    any-of (get property :anyOf)]
+                ; (prn "name is" n)
+                ; (prn "current accumulator is" accumulator)
+                ; (prn "property type is" type)
+                ; (prn "property anyof is" any-of)
+                ; (prn "is string" (string? type))
+                ; (prn "is vector" (vector? type))
+                ; (prn "is anyof" (vector? any-of))
+                (cond
+                  (string? type)
+                  (conj accumulator (str "  " (name n) ": " type))
+
+                  (vector? type)
+                  (conj accumulator (str "  " (name n) ": " (join " | " type)))
+
+                  (vector? any-of)
+                  (conj accumulator
+                        (str "  " (name n) ": "
+                             (join " | "
+                                   (map (fn [inner]
+                                          (let [type (get inner :type)
+                                                ref (get inner :$ref)]
+                                            (cond
+                                              (string? type) type
+                                              (string? ref) (make-type
+                                                             (get-definition
+                                                              (get-definition-name ref)
+                                                              (parse-json))))))
+                                        any-of)))))))
+            []
+            properties)
+           ["}"]))))
 
 (defn make-type [definition]
   (let [type (get definition :type)]
--- src/transform/test.clj
+++ src/transform/test.clj
@@ -10,46 +10,11 @@
 ; (pp/pprint (get-definition "StringSet" (parse-json)))
 
 (assert (= "string[]" (make-type (get-definition "StringSet" (parse-json)))))
-; (pp/pprint (make-type (get-definition "StringSet" (parse-json))))
 
-; (pp/pprint (parse-object {:properties {:type "string"}}))
-(println (parse-object (get-definition "AssistsConfiguration" (parse-json)) "AssistsConfiguration"))
-
-; (assert (= "type AssistConfiration = {
-;   enabled: boolean
-; }" (parse-object {:AssistConfiguration
-;                   {:type "object",
-;                    :properties
-;                    {:enabled
-;                     {:description "Whether Biome should enable assists via LSP.",
-;                      :type "boolean"}},
-;                    :additionalProperties false}})))
-
-; (pp/pprint (parse-object {:type "object",
-;                           :properties
-;                           {:enabled
-;                            {:description "Whether Biome should enable assists via LSP.",
-;                             :type "boolean"}},
-;                           :additionalProperties false}))
-;
-; (pp/pprint (parse-object {:type "object",
-;                           :properties
-;                           {:include
-;                            {:description
-;                             "A list of Unix shell style patterns. The formatter will include files/folders that will match these patterns.",
-;                             :anyOf [{:$ref "#/definitions/StringSet"} {:type "null"}]}},
-;                           :additionalProperties false}))
-;
-; (pp/pprint (parse-object {:type "object",
-;                           :properties
-;                           {:enabled
-;                            {:description "Whether Biome should enable assists via LSP.",
-;                             :type "boolean"},
-;                            :ignore
-;                            {:description "Whether Biome should enable assists via LSP.",
-;                             :type ["boolean", "string"]},
-;                            :include
-;                            {:description
-;                             "A list of Unix shell style patterns. The formatter will include files/folders that will match these patterns.",
-;                             :anyOf [{:$ref "#/definitions/StringSet"} {:type "null"}]}},
-;                           :additionalProperties false}))
+(assert (= "type AssistsConfiguration = {
+  enabled: boolean | null
+  prioritize: boolean
+  ignore: string[] | null
+  include: string[] | null
+}" (parse-object (get-definition "AssistsConfiguration" (parse-json)) "AssistsConfiguration")))
+; (println (parse-object (get-definition "AssistsConfiguration" (parse-json)) "AssistsConfiguration"))
