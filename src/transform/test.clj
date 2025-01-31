3a2cded55e2667a05312b8c7efcbf08e4e3eafc5
Nicolas Accetta <nicolas.accetta@gmail.com>
2025-01-31 09:59:31 -0300

    test: add two more cases

--- src/transform/test.clj
+++ src/transform/test.clj
@@ -34,3 +34,58 @@
   level: RulePlainConfiguration,
 }"
   (make-type (get-definition (parse-json) name-5) name-5)))
+
+(def name-6 "SuggestedExtensionMapping")
+; (println (make-type (get-definition (parse-json) name-6) name-6))
+(assert
+ (=
+  "type SuggestedExtensionMapping = {
+  component: string,
+  module: string,
+}"
+  (make-type (get-definition (parse-json) name-6) name-6)))
+
+(def name-7 "A11y")
+; (println (make-type (get-definition (parse-json) name-7) name-7))
+(assert
+ (=
+  "type A11y = {
+  useValidAnchor: RuleConfiguration | null,
+  useKeyWithClickEvents: RuleConfiguration | null,
+  useAriaPropsForRole: RuleConfiguration | null,
+  useValidLang: RuleConfiguration | null,
+  useAltText: RuleConfiguration | null,
+  noNoninteractiveTabindex: RuleFixConfiguration | null,
+  useAriaActivedescendantWithTabindex: RuleFixConfiguration | null,
+  noSvgWithoutTitle: RuleConfiguration | null,
+  noPositiveTabindex: RuleFixConfiguration | null,
+  useGenericFontNames: RuleConfiguration | null,
+  useValidAriaRole: ValidAriaRoleConfiguration | null,
+  noHeaderScope: RuleFixConfiguration | null,
+  noAriaHiddenOnFocusable: RuleFixConfiguration | null,
+  useButtonType: RuleConfiguration | null,
+  useValidAriaValues: RuleConfiguration | null,
+  useMediaCaption: RuleConfiguration | null,
+  useSemanticElements: RuleConfiguration | null,
+  useValidAriaProps: RuleFixConfiguration | null,
+  useIframeTitle: RuleConfiguration | null,
+  all: boolean | null,
+  noAccessKey: RuleFixConfiguration | null,
+  noAriaUnsupportedElements: RuleFixConfiguration | null,
+  useFocusableInteractive: RuleConfiguration | null,
+  noAutofocus: RuleFixConfiguration | null,
+  useKeyWithMouseEvents: RuleConfiguration | null,
+  noRedundantAlt: RuleConfiguration | null,
+  noLabelWithoutControl: NoLabelWithoutControlConfiguration | null,
+  noInteractiveElementToNoninteractiveRole: RuleFixConfiguration | null,
+  useHeadingContent: RuleConfiguration | null,
+  useAnchorContent: RuleFixConfiguration | null,
+  noRedundantRoles: RuleFixConfiguration | null,
+  noBlankTarget: AllowDomainConfiguration | null,
+  noDistractingElements: RuleFixConfiguration | null,
+  recommended: boolean | null,
+  noNoninteractiveElementToInteractiveRole: RuleFixConfiguration | null,
+  useHtmlLang: RuleConfiguration | null,
+}"
+  (make-type (get-definition (parse-json) name-7) name-7)))
+
